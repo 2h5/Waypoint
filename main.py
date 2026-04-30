ea82760d42f7ee1fafc731667ed93bb06ed8e66c
Patrick Hwang <lachedon@gmail.com>
2026-04-30 03:43:05 -0400

    Fixed timestamp and move command imports to top

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 
+from commands.speed import setup as speed
+from commands.sixseven import setup as sixseven
+from commands.good import setup as good
+from commands.staticmap import setup as staticmap
+from commands.route import setup as route
+from commands.translate import setup as translate
+from commands.remindme import setup as remindme
+
 load_dotenv()
 discord_token = os.getenv("DISCORD_TOKEN")
 mapbox_token = os.getenv("MAPBOX_TOKEN")
-
-
 
 intents = discord.Intents.default()
 intents.message_content = True  
@@ -18,25 +24,13 @@ tree = app_commands.CommandTree(client)
 @client.event
 async def on_ready():
     now = datetime.now()
-    timestamp = now.strftime("%b %d, %Y %H:%M%p")
+    timestamp = now.strftime("%b %d, %Y %I:%M %p")
     print(f"Logged in as {client.user} at {timestamp}")
     try:
         synced = await tree.sync()
         print(f"Synced {len(synced)} slash command(s).")
     except Exception as e:
         print(f"Error syncing commands: {e}")
-
-from commands.speed import setup as speed
-from commands.sixseven import setup as sixseven
-from commands.good import setup as good
-from commands.staticmap import setup as staticmap
-from commands.route import setup as route
-from commands.translate import setup as translate
-from commands.remindme import setup as remindme
-
-
-
-
 
 speed(tree)
 sixseven(tree)
@@ -45,8 +39,6 @@ staticmap(tree)
 route(tree)
 translate(tree)
 remindme(tree)
-
-
 
 client.run(discord_token)
 
